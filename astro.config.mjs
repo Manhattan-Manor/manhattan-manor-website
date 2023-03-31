bd0a5425920b3134b3300260a0a6911263648ab6
Axel León <yka992@hotmail.com>
2023-03-31 17:23:50 -0600

    BallDrop and NYE videos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
-
 import react from "@astrojs/react";
+
+import criticalCss from "astro-critical-css";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next(), react()]
+  integrations: [astroI18next(), react(), criticalCss()]
 });
