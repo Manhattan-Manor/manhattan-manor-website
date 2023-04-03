9aff1cb20e052fad56b6d8b190ed65cb87a04bf8
Axel León <yka992@hotmail.com>
2023-04-03 11:38:12 -0600

    Checking if criticalCss is causing the issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 import react from "@astrojs/react";
 
-import criticalCss from "astro-critical-css";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next(), react(), criticalCss()],
+  integrations: [astroI18next(), react()],
   vite: {
     ssr: {
       noExternal: ["@splidejs/react-splide"],
