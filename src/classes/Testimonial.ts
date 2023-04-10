ede377af94af471aac8abecd8e702cd68da59d18
Axel León <yka992@hotmail.com>
2023-04-10 17:18:10 -0600

    Testimonials connected to CMS

--- src/classes/Testimonial.ts
+++ src/classes/Testimonial.ts
@@ -1,19 +1,42 @@
+import i18next from "i18next";
+
 class Testimonial {
+  _id: string;
   testimonial: string;
   name: string;
   position?: string;
 
   constructor(testimonial?: Testimonial) {
     if (testimonial) {
+      this._id = testimonial._id;
       this.testimonial = testimonial.testimonial;
       this.name = testimonial.name;
       this.position = testimonial.position;
     } else {
+      this._id = "";
       this.testimonial = "";
       this.name = "";
       this.position = "";
     }
   }
+
+  public static getAll = async (): Promise<Testimonial[]> => {
+    const locale = i18next.language;
+    const response = await fetch(
+      import.meta.env.PUBLIC_CMS_API_ROUTE +
+        "/content/items/testimonials?locale=" +
+        locale,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "api-key": import.meta.env.PUBLIC_CMS_API_KEY,
+        },
+      }
+    );
+    const data = await response.json();
+    return data.map((testimonial: Testimonial) => new Testimonial(testimonial));
+  };
 }
 
 export default Testimonial;
---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,54 @@
+import React, { FC, useEffect, useState } from "react";
+import Testimonial from "../classes/Testimonial";
+import { useTranslation } from "react-i18next";
+import "../assets/styles/Testimonials.scss";
+
+const Testimonials: FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [testimonials, setTestimonials] = useState<Testimonial[]>([]);
+  const { t } = useTranslation("translation");
+
+  const init = async () => {
+    try {
+      setIsLoading(true);
+      const testimonials = await Testimonial.getAll();
+      setTestimonials(testimonials);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <section className="container mt-4" id="testimonials-section">
+      <h2 className="text-center mb-3">
+        {t("testimonials")}
+      </h2>
+
+      {isLoading && <div className="text-center">{t("common.loading")}</div>}
+      <div className="row">
+        {testimonials.map((testimonial, index) => (
+          <div
+            className="col-md-4 col-sm-6"
+            key={testimonial._id}
+            data-aos="fade-up"
+            data-aos-delay={index * 100}
+          >
+            <p className="testimonial">
+              {testimonial.testimonial}
+              <span>{testimonial.name}</span>
+              {testimonial.position && <span>{testimonial.position}</span>}
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
