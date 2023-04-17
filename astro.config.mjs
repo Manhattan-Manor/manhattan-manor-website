cec08f55034e337d5c34164af8af95688756abbd
Axel León <yka992@hotmail.com>
2023-04-17 10:48:19 -0600

    Added Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,27 @@ import react from "@astrojs/react";
 import image from "@astrojs/image";
 import critters from "astro-critters";
 
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), astroI18next(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), critters()],
+  integrations: [
+    react(),
+    astroI18next(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    critters(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   vite: {
     ssr: {
-      noExternal: ["@splidejs/react-splide"]
-    }
+      noExternal: ["@splidejs/react-splide"],
+    },
   },
-  site: "https://manhattanmanor.com/"
+  site: "https://manhattanmanor.com/",
 });
