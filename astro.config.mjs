37476549935f5a45b19328c08a652c18bd69ee94
Axel León <aleon@cnt.lat>
2024-12-05 08:51:35 -0600

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,17 @@ import astroI18next from "astro-i18next";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    astroI18next(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [react(), astroI18next(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap()],
   vite: {
     ssr: {
-      noExternal: ["@splidejs/react-splide"],
-    },
+      noExternal: ["@splidejs/react-splide"]
+    }
   },
-  site: "https://manhattanmanor.com/",
+  site: "https://manhattanmanor.com/"
 });
